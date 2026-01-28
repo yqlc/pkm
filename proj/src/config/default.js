c6fd56e13064be266f15db3ccdfdb3233f49436c
relax-happy@163.com <relax-happy@163.com>
2026-01-28 22:42:57 +0800

    移除敏感数据

--- proj/src/config/default.js
+++ proj/src/config/default.js
@@ -1,16 +1,17 @@
+require('dotenv').config();
+
 module.exports = {
   // 数据库配置
   db: {
     storage: './asset/pkm.sqlite',
-    encryptionKey: 'pKmNdjs688' // 如使用加密数据库，请设置此项
+    encryptionKey: process.env.DB_ENCRYPTION_KEY // 如使用加密数据库，请设置此项
   },
   // 邮箱监控配置
   email: {
-    user: 'relax-happy@163.com',
-    password: 'GQYPVgUdZvvXzhVy', // 注意：是IMAP授权码，不是登录密码
-    host: 'imap.163.com',
-    port: 993,
+    user: process.env.EMAIL_USER || '',
+    password: process.env.EMAIL_PASSWORD || '', // 注意：是IMAP授权码，不是登录密码
+    host: process.env.EMAIL_HOST || 'imap.163.com',
+    port: parseInt(process.env.EMAIL_PORT) || 993,
     tls: true,
-    targetSender: 'info@xx.com', // 只分析来自该发件人的邮件
   }
 };
